b53063240c9bc6470f3762d4a41968e1762c7a98
karanjarwal999 <karanjarwal999@gmail.com>
2023-07-17 13:56:53 +0530

    ok

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ const PostRouter= require('./router/PostRouter')
 const Auth= require('./authMiddleware')
 
 app.use(express.json())
+app.get('/', (req, res) => {
+    res.send('welcome!')
+})
 app.use('/users',userRouter)
 app.use('/posts',Auth,PostRouter)
 
 app.listen(process.env.PORT,async ()=>{
     console.log(`listening on port ${process.env.PORT}`)
     try {
-        mongoose.connect('mongodb+srv://karanjarwal999:Pass123@cluster0.csrraxt.mongodb.net/?retryWrites=true&w=majority')
+        mongoose.connect(process.env.MONGOURL)
         console.log('connected')
         
     } catch (error) {
